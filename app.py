2ec8865e2f9ffee502d7182e9e174cbe5881862f
Felipe Lacerda <fegolac@gmail.com>
2016-12-01 17:03:28 +0100

    Modularize layout

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template
 from parse_entry import Entry
 app = Flask(__name__)
 app.config["APPLICATION_ROOT"] = "/ddo"
@@ -6,25 +6,16 @@ app.config["APPLICATION_ROOT"] = "/ddo"
 
 @app.route("/")
 def lookup():
-    print("hi")
     query = request.args.get("query")
     select = request.args.get("select", "")
     if query is None:
-        return render_template("entry.html")
+        return render_template("default.html")
     else:
         try:
             entry = Entry(query, select)
         except ValueError:
-            return render_template("entry.html")
-        return render_template(
-            'entry.html',
-            title=entry.title,
-            pronunciations=entry.pronunciations,
-            definitions=entry.definitions,
-            faste_udtryk=entry.faste_udtryk,
-            suggestions=entry.suggestions,
-            bojning=entry.inflection
-        )
+            return render_template("default.html")
+        return render_template("definition.html", entry=entry)
 
 
 if __name__ == "__main__":
